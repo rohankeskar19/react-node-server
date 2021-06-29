f21cf8b6914f290c23f9d4ab7cc965f3601aee93
rohankeskar19 <rohankeskar89@gmail.com>
2021-06-29 16:18:04 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const buildPath = process.env.BUILD_PATH;
+
+app.use(express.static(buildPath));
+app.use((req, res, next) => {
+  res.sendFile(path.join(buildPath, "index.html"));
+});
+
+const PORT = process.env.PORT | 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port: ${PORT}`);
+});
